2b17feedf9c9e36f20499361c50efc80cd1a8e1c
Luke Parker <luke@Lukes-MacBook-Pro.local>
2019-08-29 13:36:02 -0700

    for loop

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,9 @@
 name = input('Hello! What is your name?')
 print("hello " + name + "we're going to play a little game")
 
+noun = list()
+print("input nouns")
+
+for i in range(4):
+    noun.append(input())
+    print(noun)
