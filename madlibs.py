dfd64bb1b6da8ba13d7e982fa3cad5f1ff88b3e5
Luke Parker <luke@Lukes-MacBook-Pro.local>
2019-09-02 20:13:16 -0700

    text color shift

--- madlibs.py
+++ madlibs.py
@@ -12,9 +12,9 @@ def pos_input(pos):
 name = input('Hello! What is your name? ')
 print("hello " + name + " we're going to create a little story!")
 
-print("Start by giving me a couple nouns...")
+print("Start by giving me a couple objective nouns...")
 pos_input(nouns)
-print("Now try giving me a couple verbs...")
+print("Now try giving me a couple action verbs...")
 pos_input(verbs)
 print("Almost done! Shout out some adjectives...")
 pos_input(adjectives)
@@ -28,7 +28,7 @@ shuffle(people)
 
 print("Great! Now story time!")
 
-madlib = f"""{people[0]} was afraid to go outside because of the spiders. He went online and saw a 
+madlib = f"""{people[0]} was afraid to go outside because of the spiders. They went online and saw a 
 {nouns[0]} that interested him. {people[0]} decided to buy {nouns[0]}. The next day {people[0]} went outside and {verbs[0]} the spiders 
 with {nouns[0]}. {people[0]} invited {people[1]} to look at the
 {adjectives[0]} spiders. They never feared the spiders again."""
